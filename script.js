0efbceeb2f5caa0b1c7d915c4ecae7478e769687
GuviDemo <sreeramuidesigner@gmail.com>
2024-02-27 19:36:07 +0530

    Task 5

---
+++ script.js
@@ -0,0 +1,60 @@
+let myResume={
+    "basics": {
+      "name": "SREE RAM 5",
+      "email": "sreeramuidesigner@gamil.com",
+      "phone": 7358355574,
+      "degree": "MCA",
+      "location": {
+        "address": "24A, Zaheer Hussian street, MGR nagar",
+        "postalCode": 600078,
+        "city": "Chennai",
+        "state": "Tamilnadu",
+        "country": "India"
+      },
+      "profiles": [
+        {
+          "website": "www.linkedin.com/in/sreeramuiuxdesigner",
+          "github":"https://github.com/sreeramuiuxdesigner"
+        }
+      ]
+    },
+    "work": [
+      {
+        "company": "AscendsSoft",
+        "position": "UI UX Designer",
+        "startDate": "July 2023",
+        "endDate": "Sept 2023",
+        "summary": "i am the business executive handling various task and gaining lots of experience",
+      },
+    ],
+    "education": [
+      {
+        "institution": "University of Madras",
+        "department": "MCA",
+        "studyType": "fulltime",
+        "batch start year": 2021,
+        "batch end year": 2023,
+        "gpa": 7.5,
+      }
+    ],
+    "skills": [
+      {
+        "name": "html,Bottstrap, Css, ",
+        "level": "beginer",
+        "project": [
+          "Child Monitoring App"
+        ]
+      }
+    ],
+    "languages": [
+      {
+        "language": "Tamil,English",
+      }
+    ],
+    "interests": [
+      {
+        "name": "reading, coding",
+      }
+    ]
+  }
+  console.log(myResume);
